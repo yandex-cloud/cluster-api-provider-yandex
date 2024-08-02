e65a21cbc88b5f4bc9a5b65af34b15ef4f7afeb4
pavelgorkin <pavelgorkin@yandex-team.ru>
2024-08-02 10:58:56 +0200

    Some more labels

--- internal/pkg/cloud/scope/labels.go
+++ internal/pkg/cloud/scope/labels.go
@@ -8,27 +8,43 @@ import (
 )
 
 const (
-	// Pair of labels used in Yandex analytics to differentiate VMs created by CAPI
+	// Labels used in Yandex analytics to differentiate VMs created by CAPI
 	// CAPY means "Cluster API Provider Yandex"
-	yaAnalyticsClusterIdLabel   string = "capy-cluster-id"
-	yaAnalyticsNodeGroupIdLabel string = "capy-node-group-id"
+
+	// yaAnalyticsClusterNameLabel representation of CAPI "cluster.x-k8s.io/cluster-name" label
+	yaAnalyticsClusterNameLabel string = "yandex.cloud/capy-cluster-name"
+	// yaAnalyticsFolderIdLabel Yandex Cloud folder Id
+	yaAnalyticsFolderIdLabel string = "yandex.cloud/folderID"
+	// yaAnalyticsControlPlaneNameLabel representation of CAPI "cluster.x-k8s.io/control-plane-name" label. Absent for workers
+	yaAnalyticsControlPlaneNameLabel string = "yandex.cloud/capy-control-plane-name"
+	// yaAnalyticsMachineDeploymentNameLabel representation of CAPI "cluster.x-k8s.io/deployment-name" label. Absent for masters
+	yaAnalyticsMachineDeploymentNameLabel string = "yandex.cloud/capy-cluster-machine-deployment-name"
+	// yaAnalyticsClusterHashLabel label value is the md5(folderId + clusterName) truncated to 20s
+	yaAnalyticsClusterHashLabel string = "yandex.cloud/capy-cluster-hash"
+	// yaAnalyticsMachineDeploymentLabel label value is the md5(folderId + clusterName + deploymentName) truncated to 20s
+	yaAnalyticsMachineDeploymentLabel string = "yandex.cloud/capy-cluster-machine-deployment-hash"
 )
 
 // getMachineLabels prepares labels for machine in current scope
 func (m *MachineScope) getMachineLabels() map[string]string {
 	labels := map[string]string{
-		"managed-by": "capy-controller-manager",
-		"purpose":    "capy-test",
+		"managed-by":                "capy-controller-manager",
+		"purpose":                   "capy-test",
+		yaAnalyticsClusterNameLabel: m.YandexMachine.Labels[clusterv1.ClusterNameLabel],
+		yaAnalyticsFolderIdLabel:    m.YandexMachine.Spec.FolderID,
 	}
-	// Label value is the md5(folderId + clusterName) truncated to 20s
-	labels[yaAnalyticsClusterIdLabel] = getYaAnalyticsLabelValue(m.YandexMachine.Spec.FolderID, m.YandexMachine.Labels[clusterv1.ClusterNameLabel])
-
-	// Label value is the md5(folderId + clusterName + deploymentName) truncated to 20s
-	deploymentName := m.YandexMachine.Labels[clusterv1.MachineDeploymentNameLabel]
-	if deploymentName == "" {
+	deploymentName := ""
+	if m.YandexMachine.Labels[clusterv1.MachineControlPlaneNameLabel] != "" {
+		labels[yaAnalyticsControlPlaneNameLabel] = m.YandexMachine.Labels[clusterv1.MachineControlPlaneNameLabel]
 		deploymentName = m.YandexMachine.Labels[clusterv1.MachineControlPlaneNameLabel]
 	}
-	labels[yaAnalyticsNodeGroupIdLabel] = getYaAnalyticsLabelValue(m.YandexMachine.Spec.FolderID, m.YandexMachine.Labels[clusterv1.ClusterNameLabel], deploymentName)
+	if m.YandexMachine.Labels[clusterv1.MachineDeploymentNameLabel] != "" {
+		labels[yaAnalyticsMachineDeploymentNameLabel] = m.YandexMachine.Labels[clusterv1.MachineDeploymentNameLabel]
+		deploymentName = m.YandexMachine.Labels[clusterv1.MachineDeploymentNameLabel]
+	}
+
+	labels[yaAnalyticsClusterHashLabel] = getYaAnalyticsLabelValue(m.YandexMachine.Spec.FolderID, m.YandexMachine.Labels[clusterv1.ClusterNameLabel])
+	labels[yaAnalyticsMachineDeploymentLabel] = getYaAnalyticsLabelValue(m.YandexMachine.Spec.FolderID, m.YandexMachine.Labels[clusterv1.ClusterNameLabel], deploymentName)
 
 	return labels
 }
